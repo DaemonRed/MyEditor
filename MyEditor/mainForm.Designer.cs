b110ceca0753880b8e9462eb7d8ecd723dd19568
Aziz Rafsanjani <coded_raf@yahoo.com>
2016-07-11 10:51:01 +0000

    initial commit

---
+++ MyEditor/mainForm.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace NewApp
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.rtbTextArea = new System.Windows.Forms.RichTextBox();
+            this.menuBar = new System.Windows.Forms.MenuStrip();
+            this.fileMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuItemFind = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemReplace = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemSelectAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemDate = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusBarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.boldToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.italicsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontColourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundColourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.visualStyler1 = new SkinSoft.VisualStyler.VisualStyler(this.components);
+            this.lblLineCount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblWordCount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuBar.SuspendLayout();
+            this.statusBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.visualStyler1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rtbTextArea
+            // 
+            this.rtbTextArea.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbTextArea.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rtbTextArea.Location = new System.Drawing.Point(12, 27);
+            this.rtbTextArea.Name = "rtbTextArea";
+            this.rtbTextArea.Size = new System.Drawing.Size(580, 319);
+            this.rtbTextArea.TabIndex = 0;
+            this.rtbTextArea.Text = "";
+            // 
+            // menuBar
+            // 
+            this.menuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenu,
+            this.editMenu,
+            this.viewMenu,
+            this.formatMenu});
+            this.menuBar.Location = new System.Drawing.Point(0, 0);
+            this.menuBar.Name = "menuBar";
+            this.menuBar.Size = new System.Drawing.Size(592, 24);
+            this.menuBar.TabIndex = 1;
+            this.menuBar.Text = "menuStrip1";
+            // 
+            // fileMenu
+            // 
+            this.fileMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileMenu.Name = "fileMenu";
+            this.fileMenu.Size = new System.Drawing.Size(37, 20);
+            this.fileMenu.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editMenu
+            // 
+            this.editMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemCopy,
+            this.menuItemCut,
+            this.menuItemPaste,
+            this.menuItemDelete,
+            this.toolStripSeparator1,
+            this.menuItemFind,
+            this.menuItemReplace,
+            this.menuItemSelectAll,
+            this.menuItemDate});
+            this.editMenu.Name = "editMenu";
+            this.editMenu.Size = new System.Drawing.Size(39, 20);
+            this.editMenu.Text = "Edit";
+            // 
+            // menuItemCopy
+            // 
+            this.menuItemCopy.Name = "menuItemCopy";
+            this.menuItemCopy.Size = new System.Drawing.Size(162, 22);
+            this.menuItemCopy.Text = "Copy";
+            // 
+            // menuItemCut
+            // 
+            this.menuItemCut.Name = "menuItemCut";
+            this.menuItemCut.Size = new System.Drawing.Size(162, 22);
+            this.menuItemCut.Text = "Cut";
+            // 
+            // menuItemPaste
+            // 
+            this.menuItemPaste.Name = "menuItemPaste";
+            this.menuItemPaste.Size = new System.Drawing.Size(162, 22);
+            this.menuItemPaste.Text = "Paste";
+            // 
+            // menuItemDelete
+            // 
+            this.menuItemDelete.Name = "menuItemDelete";
+            this.menuItemDelete.Size = new System.Drawing.Size(162, 22);
+            this.menuItemDelete.Text = "Delete";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(159, 6);
+            // 
+            // menuItemFind
+            // 
+            this.menuItemFind.Name = "menuItemFind";
+            this.menuItemFind.Size = new System.Drawing.Size(162, 22);
+            this.menuItemFind.Text = "Find";
+            // 
+            // menuItemReplace
+            // 
+            this.menuItemReplace.Name = "menuItemReplace";
+            this.menuItemReplace.Size = new System.Drawing.Size(162, 22);
+            this.menuItemReplace.Text = "Replace";
+            // 
+            // menuItemSelectAll
+            // 
+            this.menuItemSelectAll.Name = "menuItemSelectAll";
+            this.menuItemSelectAll.Size = new System.Drawing.Size(162, 22);
+            this.menuItemSelectAll.Text = "Select All";
+            // 
+            // menuItemDate
+            // 
+            this.menuItemDate.Name = "menuItemDate";
+            this.menuItemDate.Size = new System.Drawing.Size(162, 22);
+            this.menuItemDate.Text = "Insert Date/Time";
+            // 
+            // viewMenu
+            // 
+            this.viewMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusBarToolStripMenuItem});
+            this.viewMenu.Name = "viewMenu";
+            this.viewMenu.Size = new System.Drawing.Size(44, 20);
+            this.viewMenu.Text = "View";
+            // 
+            // statusBarToolStripMenuItem
+            // 
+            this.statusBarToolStripMenuItem.Name = "statusBarToolStripMenuItem";
+            this.statusBarToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.statusBarToolStripMenuItem.Text = "Status Bar";
+            // 
+            // formatMenu
+            // 
+            this.formatMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.boldToolStripMenuItem,
+            this.italicsToolStripMenuItem,
+            this.fontColourToolStripMenuItem,
+            this.backgroundColourToolStripMenuItem});
+            this.formatMenu.Name = "formatMenu";
+            this.formatMenu.Size = new System.Drawing.Size(57, 20);
+            this.formatMenu.Text = "Format";
+            // 
+            // boldToolStripMenuItem
+            // 
+            this.boldToolStripMenuItem.Name = "boldToolStripMenuItem";
+            this.boldToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.boldToolStripMenuItem.Text = "Bold";
+            // 
+            // italicsToolStripMenuItem
+            // 
+            this.italicsToolStripMenuItem.Name = "italicsToolStripMenuItem";
+            this.italicsToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.italicsToolStripMenuItem.Text = "Italics";
+            // 
+            // fontColourToolStripMenuItem
+            // 
+            this.fontColourToolStripMenuItem.Name = "fontColourToolStripMenuItem";
+            this.fontColourToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.fontColourToolStripMenuItem.Text = "Font Colour";
+            // 
+            // backgroundColourToolStripMenuItem
+            // 
+            this.backgroundColourToolStripMenuItem.Name = "backgroundColourToolStripMenuItem";
+            this.backgroundColourToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.backgroundColourToolStripMenuItem.Text = "BG Color";
+            // 
+            // statusBar
+            // 
+            this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblLineCount,
+            this.lblWordCount});
+            this.statusBar.Location = new System.Drawing.Point(0, 349);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(592, 22);
+            this.statusBar.TabIndex = 2;
+            this.statusBar.Text = "statusStrip1";
+            // 
+            // visualStyler1
+            // 
+            this.visualStyler1.HostForm = this;
+            this.visualStyler1.License = ((SkinSoft.VisualStyler.Licensing.VisualStylerLicense)(resources.GetObject("visualStyler1.License")));
+            this.visualStyler1.LoadVisualStyle(null, "OSX (Tiger).vssf");
+            // 
+            // lblLineCount
+            // 
+            this.lblLineCount.Name = "lblLineCount";
+            this.lblLineCount.Size = new System.Drawing.Size(37, 17);
+            this.lblLineCount.Text = "Lines:";
+            // 
+            // lblWordCount
+            // 
+            this.lblWordCount.Name = "lblWordCount";
+            this.lblWordCount.Size = new System.Drawing.Size(44, 17);
+            this.lblWordCount.Text = "Words:";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 371);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.rtbTextArea);
+            this.Controls.Add(this.menuBar);
+            this.MainMenuStrip = this.menuBar;
+            this.Name = "mainForm";
+            this.Text = "MyEditor v0.1";
+            this.menuBar.ResumeLayout(false);
+            this.menuBar.PerformLayout();
+            this.statusBar.ResumeLayout(false);
+            this.statusBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.visualStyler1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbTextArea;
+        private System.Windows.Forms.MenuStrip menuBar;
+        private System.Windows.Forms.ToolStripMenuItem fileMenu;
+        private System.Windows.Forms.ToolStripMenuItem editMenu;
+        private System.Windows.Forms.ToolStripMenuItem viewMenu;
+        private System.Windows.Forms.ToolStripMenuItem formatMenu;
+        private System.Windows.Forms.StatusStrip statusBar;
+        private SkinSoft.VisualStyler.VisualStyler visualStyler1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem statusBarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem boldToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem italicsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontColourToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backgroundColourToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuItemCopy;
+        private System.Windows.Forms.ToolStripMenuItem menuItemCut;
+        private System.Windows.Forms.ToolStripMenuItem menuItemPaste;
+        private System.Windows.Forms.ToolStripMenuItem menuItemDelete;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem menuItemFind;
+        private System.Windows.Forms.ToolStripMenuItem menuItemReplace;
+        private System.Windows.Forms.ToolStripMenuItem menuItemSelectAll;
+        private System.Windows.Forms.ToolStripMenuItem menuItemDate;
+        private System.Windows.Forms.ToolStripStatusLabel lblLineCount;
+        private System.Windows.Forms.ToolStripStatusLabel lblWordCount;
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,30 @@
+# MY EDITOR
+
+TODO: A simple C# based text editor to give programmers of UEW-K some knowledge on how to do collaborative work
+
+## Installation
+
+Just launch MyEditor.sln and start adding features
+
+## Usage
+
+
+
+TODO: Write usage instructions
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+
+## Credits
+
+TODO: Write credits
+
+## License
+
+TODO: Write license
